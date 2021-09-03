a82186c69f48a9b952fb07d3d257609fe3d0f891
Çetin ARDAL <kral2@users.noreply.github.com>
2021-09-03 19:43:59 +0200

    feat(show-hidden-files): add usage information

--- system-config/show-hidden-files.sh
+++ system-config/show-hidden-files.sh
@@ -2,11 +2,16 @@
 
 # Show hidden files and relaunch Finder
 
-if [ -z $1 ]; then
-	action="true"
+# Usage:
+# > ./show-hidden-files.sh [true|false]
+
+if [ -z "$1" ]; then
+	echo ""
+	echo "Usage information: ./show-hidden-files.sh [true|false]"
+	echo "- true: show hidden files"
+	echo "- false: mask hidden files"
 else
 	action=$1
+	defaults write com.apple.Finder AppleShowAllFiles "${action}"
+    killall Finder
 fi
-
-defaults write com.apple.Finder AppleShowAllFiles ${action}
-killall Finder
